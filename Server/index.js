6e0aafcddb4f9e2d73c02b2779b7adf6a1dd22d0
Sooraj-Rao <143273952+Sooraj-Rao@users.noreply.github.com>
2024-04-24 18:35:23 +0530

    #S56YHUIOK5

--- Server/index.js
+++ Server/index.js
@@ -6,7 +6,11 @@ import { router } from "./src/Router.js";
 const app = express();
 Connect();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://qucik-link.vercel.app",
+  })
+);
 app.use(router);
 
 app.listen(3000, () => console.log("Server started"));
