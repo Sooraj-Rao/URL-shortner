e7fc07f475342b23eb950317240fb3599d7a1ccd
Sooraj-Rao <143273952+Sooraj-Rao@users.noreply.github.com>
2024-06-28 22:21:06 +0530

    (fix:db)

--- Server/index.js
+++ Server/index.js
@@ -5,9 +5,15 @@ import { router } from "./src/Router.js";
 
 const app = express();
 Connect();
-
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://srj-quicklink.vercel.app/", "http://localhost:5173/"],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 app.use(router);
 
 app.listen(3000, () => console.log("Server started"));
