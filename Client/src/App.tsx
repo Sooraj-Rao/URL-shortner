024b3918f9ae58dfd6d31d6d5cb33859b37ed091
Sooraj-Rao <143273952+Sooraj-Rao@users.noreply.github.com>
2024-06-29 15:28:09 +0530

    ( update )

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,4 +1,5 @@
 import { Toaster } from "./components/ui/toaster";
+import { SomeData } from "./data/linkData";
 import Home from "./main/Module/home";
 import NotFound from "./main/components/NotFound";
 import Header from "./main/components/header";
@@ -6,13 +7,6 @@ import { ThemeProvider } from "./main/components/theme.provider";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
-  const SomeData = {
-    contact:
-      "https://contact.soorajrao.in?mode=report&app=quicklink&utm_source=quicklink_header",
-    github: "https://github.com/Sooraj-Rao/URL-shortner",
-    main: "https://soorajrao.in",
-  };
-
   return (
     <BrowserRouter>
       <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
